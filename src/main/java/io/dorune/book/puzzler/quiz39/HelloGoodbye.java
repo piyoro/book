6c07d4f45dea94092443a3c8e13db07f849285f0
piyor <piyor@DESKTOP-2M514K4>
2020-03-29 22:02:53 +0900

    finally 구문의 Goodbye world 를 출력하기 위한 개선된 처리 방식 추가

--- src/main/java/io/dorune/book/puzzler/quiz39/HelloGoodbye.java
+++ src/main/java/io/dorune/book/puzzler/quiz39/HelloGoodbye.java
@@ -9,4 +9,18 @@ public class HelloGoodbye {
             System.out.println("Goodbye world");
         }
     }
+    
+    /**
+     * finally 구문의 Goodbye world 를 출력하기 위한 개선된 처리 방식
+     */
+    public static void improve() {
+    	System.out.println("Hello world");
+    	Runtime.getRuntime().addShutdownHook(new Thread() {
+    		public void run() {
+    			System.out.println("Goodbye world");
+    		}
+    	});
+    	System.exit(0);
+    }
+    
 }
